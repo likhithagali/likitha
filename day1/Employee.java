8619dd2c0188868c7d792634ddd14d4c19710f8c
likhithagali <likithagali2003@gmail.com>
2024-11-09 20:37:35 +0530

    Add files via upload

---
+++ day1/Employee.java
@@ -0,0 +1,12 @@
+public class Employee
+{
+public static void main(String args[])
+{
+ int eno=65;
+ String ename="LIKHITHA";
+ float esal=15000f;
+System.out.println("given employee number is:"+eno);
+System.out.println("given employee name is:"+ename);
+System.out.println("given employee salary is:"+esal);
+}
+}
---
+++ day1/Product.java
@@ -0,0 +1,12 @@
+class Product
+{
+public static void main(String args[])
+{
+ int pno=65;
+ String pname="ice cream";
+ float pcost=200f;
+System.out.println("given product number is:"+pno);
+System.out.println("given product name is:"+pname);
+System.out.println("given product cost is:"+pcost);
+}
+}
